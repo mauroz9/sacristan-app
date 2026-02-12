97513b1f2c6725003b4de86222b25cb992091526
Mauro Serrano <mauro.serranoo26@gmail.com>
2026-02-12 18:41:08 +0100

    header que contiene datos de la secuencia y la barra de progreso

---
+++ lib/features/play_sequence/ui/widgets/header_info.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:pantalla_login_ui/features/play_sequence/ui/widgets/sequence_progress_bar.dart';
+
+class HeaderInfo extends StatelessWidget {
+  final int totalSteps;
+  final int currentStep;
+  final String title;
+
+  const HeaderInfo({
+    super.key,
+    required this.totalSteps,
+    required this.currentStep,
+    this.title = "Lavado de Manos",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final percentage = ((currentStep / totalSteps) * 100).round();
+    final progress = currentStep / totalSteps;
+
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+              IconButton.filled(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                    const Color.fromARGB(78, 10, 10, 10),
+                  ),
+                ),
+                icon: Icon(
+                  Icons.close,
+                  size: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Paso $currentStep de $totalSteps",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                "$percentage%",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          SequenceProgressBar(totalSteps: totalSteps, currentStep: currentStep)
+        ],
+      ),
+    );
+  }
+}
