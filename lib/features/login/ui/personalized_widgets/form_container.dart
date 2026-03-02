7fb0d0126164f65c6019197965c3fd9897b48dfd
mauroz9 <mauro.serranoo26@gmail.com>
2026-03-02 14:26:24 +0100

    feat: aplicación del bloc para el inicio de sesión asi como el key para validar los campos del formulario

--- lib/features/login/ui/personalized_widgets/form_container.dart
+++ lib/features/login/ui/personalized_widgets/form_container.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pantalla_login_ui/core/services/auth_service.dart';
+import 'package:pantalla_login_ui/features/login/bloc/login_bloc.dart';
 
 class FormContainer extends StatefulWidget {
   const FormContainer({super.key});
@@ -8,133 +11,192 @@ class FormContainer extends StatefulWidget {
 }
 
 class _FormContainerState extends State<FormContainer> {
-  bool _isPasswordVisible = false;
   final _formKey = GlobalKey<FormState>();
-  final _emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+  final _emailRegex = RegExp(
+    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+  );
+  late String _email;
+  late String _password;
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Container(
-        width: 400,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
-        margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-      
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          spacing: 20,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 5,
-              children: [
-                Text(
-                  "Email",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 94, 94, 94),
-                  ),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if(value == null || value.isEmpty){
-                      return 'Este campo es obligatorio';
-                    }else if(!_emailRegex.hasMatch(value)){
-                      return 'Introduce una dirección email válida';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person_outline_outlined),
-                    focusColor: Color.fromRGBO(31, 60, 139, 1.0),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(color: Colors.grey, width: 1),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(color: Color.fromRGBO(31, 60, 139, 1.0), width: 2),
-                    ),
-                    hintText: 'Introduce tu email de usuario',
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 5,
-              children: [
-                Text(
-                  "Contraseña",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 94, 94, 94),
-                  ),
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if(value == null || value.isEmpty){
-                      return 'Este campo es obligatorio';
-                    }
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock_outline_rounded),
-                    focusColor: Color.fromRGBO(31, 60, 139, 1.0),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(color: Colors.grey, width: 1)
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(color: Color.fromRGBO(31, 60, 139, 1.0), width: 2),
-                    ),
-                    hintText: 'Introduce tu contraseña',
-                  ),
-                ),
-                Text(
-                  "¿Olvidaste tu contraseña?",
-                  style: TextStyle(
-                    fontSize: 14,
-                    color:Color.fromRGBO(31, 60, 139, 1.0)
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 50,),
-            ElevatedButton(
-              onPressed: () {if(_formKey.currentState!.validate()){
-                Navigator.pushReplacementNamed(context, '/home');
-              }},
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(
-                  Color.fromRGBO(31, 60, 139, 1.0),
-                ),
-                shape: WidgetStatePropertyAll(
-                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                ),
-                minimumSize: WidgetStatePropertyAll(Size(double.infinity, 50)),
+    return BlocProvider(
+      create: (context) => LoginBloc(AuthService()),
+      child: Builder(
+        builder: (innerContext) => BlocListener<LoginBloc, LoginState>(
+          listener: (context, state) {
+            if (state is LoginError) {
+              Navigator.pop(context);
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.message)));
+            } else if (state is LoginLoading) {
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) =>
+                    const Center(child: CircularProgressIndicator()),
+              );
+            } else if (state is LoginSuccess) {
+              Navigator.pop(context);
+              Navigator.pushReplacementNamed(context, '/home');
+            }
+          },
+          child: Form(
+            key: _formKey,
+            child: Container(
+              width: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
               ),
-              child: Text(
-                "Iniciar Sesión",
-                style: TextStyle(color: Colors.white, fontSize: 20),
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 20,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 5,
+                    children: [
+                      Text(
+                        "Email",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 94, 94, 94),
+                        ),
+                      ),
+                      TextFormField(
+                        onChanged: (value) {
+                          _email = value;
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Este campo es obligatorio';
+                          } else if (!_emailRegex.hasMatch(value)) {
+                            return 'Introduce una dirección email válida';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person_outline_outlined),
+                          focusColor: Color.fromRGBO(31, 60, 139, 1.0),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(31, 60, 139, 1.0),
+                              width: 2,
+                            ),
+                          ),
+                          hintText: 'Introduce tu email de usuario',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 5,
+                    children: [
+                      Text(
+                        "Contraseña",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 94, 94, 94),
+                        ),
+                      ),
+                      TextFormField(
+                        onChanged: (value) {
+                          _password = value;
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Este campo es obligatorio';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_outline_rounded),
+                          focusColor: Color.fromRGBO(31, 60, 139, 1.0),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(31, 60, 139, 1.0),
+                              width: 2,
+                            ),
+                          ),
+                          hintText: 'Introduce tu contraseña',
+                        ),
+                      ),
+                      Text(
+                        "¿Olvidaste tu contraseña?",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Color.fromRGBO(31, 60, 139, 1.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        innerContext.read<LoginBloc>().add(
+                          LoginRequested(email: _email, password: _password),
+                        );
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        Color.fromRGBO(31, 60, 139, 1.0),
+                      ),
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      minimumSize: WidgetStatePropertyAll(
+                        Size(double.infinity, 50),
+                      ),
+                    ),
+                    child: Text(
+                      "Iniciar Sesión",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Text(
+                    "¿Necesitas ayuda? Contacta con tu profesor",
+                    style: TextStyle(color: Color.fromARGB(255, 94, 94, 94)),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 50,),
-            Text("¿Necesitas ayuda? Contacta con tu profesor", style: TextStyle(color: Color.fromARGB(255, 94, 94, 94)),)
-          ],
+          ),
         ),
       ),
     );
