6c3c4b3cc2698f9812d7441194dcc81bb8683472
mauroz9 <mauro.serranoo26@gmail.com>
2026-03-02 14:25:27 +0100

    feat: bloc para la gestión del login

---
+++ lib/features/login/bloc/login_bloc.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:pantalla_login_ui/core/models/login_request_model.dart';
+import 'package:pantalla_login_ui/core/models/login_response_model.dart';
+import 'package:pantalla_login_ui/core/services/auth_service.dart';
+import 'package:pantalla_login_ui/core/services/token_storage_service.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginBloc(AuthService authService) : super(LoginInitial()) {
+    on<LoginRequested>((event, emit) async {
+      emit(LoginLoading());
+
+      if (event.email.trim().isEmpty || event.password.trim().isEmpty) {
+        emit(LoginError(message: "Por favor, completa todos los campos."));
+        return;
+      }
+
+      try{
+        final response = await authService.login(LoginRequest(email: event.email, password: event.password),).timeout(const Duration(seconds: 10));
+
+        final storage = TokenStorage();
+        await storage.saveToken(response.token);
+
+        emit(LoginSuccess(response: response));
+      }on AuthException catch (e){
+        emit(LoginError(message: e.message));
+      }catch (e){
+        emit(LoginError(message: "Ocurrio un error inesperado: $e"));
+      }
+    });
+  }
+}
