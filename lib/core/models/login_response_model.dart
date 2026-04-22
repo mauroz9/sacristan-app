561562995e89fc0f5c1f2b25164fac6e8e166c17
Kuro-0-0 <145166612+Kuro-0-0@users.noreply.github.com>
2026-04-22 17:32:05 +0200

    feat: minor tweaks

--- lib/core/models/login_response_model.dart
+++ lib/core/models/login_response_model.dart
@@ -1,47 +1,31 @@
 class LoginResponse {
   final String email;
-  final int id;
-  final String lastName;
-  final String name;
   final String refreshToken;
   final List<String> roles;
   final String token;
-  final String username;
 
   LoginResponse({
     required this.email,
-    required this.id,
-    required this.lastName,
-    required this.name,
     required this.refreshToken,
     required this.roles,
     required this.token,
-    required this.username,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
     return LoginResponse(
       email: json['email'] as String,
-      id: json['id'] as int,
-      lastName: json['lastName'] as String,
-      name: json['name'] as String,
       refreshToken: json['refreshToken'] as String,
       roles: List<String>.from(json['roles'] as List),
       token: json['token'] as String,
-      username: json['username'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'email': email,
-      'id': id,
-      'lastName': lastName,
-      'name': name,
       'refreshToken': refreshToken,
       'roles': roles,
       'token': token,
-      'username': username,
     };
   }
 }
